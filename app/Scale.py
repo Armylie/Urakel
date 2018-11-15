3b4a41ab2521293de34fc75cf2dab090f78338ef
ssteinsiek <s.steinsiek@web.de>
2018-11-15 16:19:34 +0100

    aufgeräumt und erste Ideen zu #1

--- app/Scale.py
+++ app/Scale.py
@@ -1,1 +1,13 @@
-bpy.context.object.dimensions[2] = Z
+def changeDim(X,Y,Z):
+    bpy.context.object.dimensions[0] = X
+    bpy.context.object.dimensions[1] = Y
+    bpy.context.object.dimensions[2] = Z
+
+def changeHeight(Z):
+    bpy.context.object.dimensions[2] = Z
+    
+def changeScaled(X):
+    bpy.context.object.dimensions[0] = X
+    # TODO: get oldX Dimension, get oldY Dimension
+    # Y = oldY/oldX * X
+    bpy.context.object.dimension[1] = Y
