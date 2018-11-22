93ac638ab28c8a44dae1cfd01570579024fdfe61
ssteinsiek <s.steinsiek@web.de>
2018-11-22 14:15:33 +0100

    fixed Background Mode Problems refs #1

--- app/Scale.py
+++ app/Scale.py
@@ -8,21 +8,26 @@ def initialize(inpath):
     bpy.ops.object.delete(use_global=False)
     bpy.ops.import_mesh.stl(filepath=inpath)
 
-# TODO: Funkioniert scheinbar nur bei direkt eingabe, nicht über Python Skript im Blind Modus :o
 
 def changeDim(X,Y,Z):
     bpy.context.object.dimensions[0] = X
+    bpy.context.scene.update()
     bpy.context.object.dimensions[1] = Y
+    bpy.context.scene.update()
     bpy.context.object.dimensions[2] = Z
+    bpy.context.scene.update()
 
 def changeHeight(Z):
     bpy.context.object.dimensions[2] = Z
+    bpy.context.scene.update()
 
 def changeScaled(X):
     oldX = bpy.context.object.dimensions[0]
     oldY = bpy.context.object.dimensions[1]
     bpy.context.object.dimensions[0] = X
+    bpy.context.scene.update()
     bpy.context.object.dimension[1] = oldY/oldX * X
+    bpy.context.scene.update()
 
 def getValues():
     return [bpy.context.object.dimensions[0],bpy.context.object.dimensions[1],bpy.context.object.dimensions[2]]
@@ -32,8 +37,7 @@ if __name__ == "__main__":
 
     # TODO: anders Steuern welche Funktion ausgeführt wird
     if sys.argv[5] == '0':
-        print(0)
-        changeDim(int(sys.argv[6]),int(sys.argv[7]),int(sys.argv[8]))
+        changeDim(float(sys.argv[6]),float(sys.argv[7]),float(sys.argv[8]))
     elif sys.argv[5] == 1:
         changeHeight(sys.argv[6])
     elif sys.argv[5] == 2:
